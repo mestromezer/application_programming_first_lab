aac27ead1225287ec26eb8dc9dbaca05dbc38e4d
mestromezer <94231152+mestromezer@users.noreply.github.com>
2022-09-16 11:10:20 +0400

    First working prototype
    Things to do:
    -Count marks in parts
    -FInd the comment itself (not necessary)

--- script.py
+++ script.py
@@ -1,53 +1,64 @@
+from msilib.schema import File
 import requests
 from bs4 import BeautifulSoup as BS
 import sys
+sys.getdefaultencoding()
+'ascii'
+sys.getfilesystemencoding()
+'UTF-8'
 import os
-import re
-# -*- coding: cp1251 -*-
-
-class FileManager:
-    def __init__(self, name, mode):
-        self.file = open(name, mode)
-        self.stdout = sys.stdout
-
-    def __del__(self):
-        self.close()
-
-    def write(self, data):
-        self.stdout.write(data)
-        self.file.write(data)
-
-    def flush(self):
-        self.stdout.flush()
-        self.file.flush()
-
-    def close(self):
-        if sys.stdout is self:
-            sys.stdout = self.stdout
-        self.file.close()
+import time
+        
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+}
     
 def create_repo():
     os.rmdir("dataset")
     os.mkdir("dataset")
     for i in range(1,6):
         os.mkdir("dataset/"+str(i))
-
-def set_connection(url):
+        
+def get_page(url):
     r = requests.post(url,headers={"User-Agent":"Mozilla/5.0"})
-    print(r.status_code)
-    if r.status_code == 200:
-        return r.text
-    else:
-        return 0
-    
-def urls_to_comments(soup):
-    return soup.find_all('a','class'=='review-btn review-read-link')
-        
-        
-        
+    #print(r.text)
+    time.sleep(3)
+    if(r.status_code == 200) : return BS(r.content,'html.parser')
+    else : return -1
+
 if __name__=="__main__":
-    url = input("Укажите URL: ")
-    content = set_connection(url)
-    soup = BS(content)
-    urls = urls_to_comments(soup)
-    print(urls)
+    
+    html = get_page('https://www.livelib.ru/reviews/~2#reviews')
+    
+    if html == -1 : 
+        print("No connection")
+        exit()
+        
+    marks = list()
+    
+    for item in html.findAll('span',class_='lenta-card__mymark'):
+        marks.append(item.text.strip())
+        
+    names = list()
+    
+    for item in html.findAll('a', class_='lenta-card__book-title'):
+        names.append(item.text.strip())
+        
+    comments = list()
+    
+    for item in html.findAll('div',id='lenta-card__text-review-escaped'):
+        comments.append(item.text.strip())
+    
+    print(marks)
+    
+    print(names)
+    
+    outFile = open('output.xml', 'wb')
+    for comment in comments:
+        outFile.write(comment.encode('utf-8'))
+        outFile.write('\n\n\n'.encode('utf-8'))
+    
+    
+            
+    
+        
