49e2828730fe24df2d1d72ea43904ed2869ed749
mestromezer <global0908@mail.ru>
2022-09-21 23:17:38 +0400

    smth

--- script.py
+++ script.py
@@ -1,103 +1,122 @@
+from fileinput import filename
 from msilib.schema import File
 import requests
 from bs4 import BeautifulSoup as BS
 import sys
-sys.getdefaultencoding()
-'ascii'
-sys.getfilesystemencoding()
-'UTF-8'
 import os
 import time
+
+class Comment:
+    
+    def __init__(self,name,comment,mark):
+        if name != '': self.name = name
+        if comment!='': self.comment = comment
+        if mark<=5 and mark >= 0: self.mark = mark
+    
+    def print_values(self):
+        print(f"Name:{self.name}\nMark: {self.mark}\nComment: {self.comment}")
         
 headers = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+    'accept': '*/*',
+    'User-Agent':'Mozilla/5.0'
 }
     
 def create_repo():
-    os.rmdir("dataset")
     os.mkdir("dataset")
     for i in range(1,6):
         os.mkdir("dataset/"+str(i))
         
 def get_page(url):
-    r = requests.post(url,headers={"User-Agent":"Mozilla/5.0"})
-    #print(r.text)
+    r = requests.get(url,headers=headers)
     time.sleep(3)
     if(r.status_code == 200) : return BS(r.content,'html.parser')
     else : return -1
+    
+def get_marks(items):
+    marks = list()
+    for item in items:
+            marks.append(item.text.strip())
+    return marks
 
+def get_names(items):
+    names = list()
+    for item in items:
+        names.append(item.text.strip())
+    return names
+        
+def get_comments(items):
+    comments = list()
+    for item in items:
+        comments.append(item.text.strip())
+    return comments
+        
+def get_last_file_name(path):
+    files = os.listdir(path)
+    if files: return files[files.__len__()]
+    
 if __name__=="__main__":
     
     url = 'https://www.livelib.ru/reviews/'
     
-    dataset = dict()
-    zero = 0
-    one = 0
-    two = 0
-    three = 0
-    four = 0
-    five = 0
+    dataset = list()
+    one=0
+    two=0
+    three=0
+    four=0
+    five=0
+    
+    least_num_of_marks = 1
+    max_num_of_requests = 7000
     
     html = get_page(url)
-    
     if html == -1 : 
             print("No connection")
             exit()
             
-    for i in range (1,9999):
+    for i in range (1,max_num_of_requests):
+        
+        if one>=least_num_of_marks and two>=least_num_of_marks and three>=least_num_of_marks and four>=least_num_of_marks and five>=least_num_of_marks: 
+            i=max_num_of_requests
+        
         html = get_page(url+'~'+str(i)+'#reviews')
         if html == -1 : break
         
+        marks = get_marks(html.findAll('span',class_='lenta-card__mymark'))
         
-        marks = list()
-    
-        for item in html.findAll('span',class_='lenta-card__mymark'):
-            marks.append(item.text.strip())
+        names = get_names(html.findAll('a', class_='lenta-card__book-title'))
         
-        names = list()
-    
-        for item in html.findAll('a', class_='lenta-card__book-title'):
-            names.append(item.text.strip())
-        
-        comments = list()
-    
-        for item in html.findAll('div',id='lenta-card__text-review-escaped'):
-            comments.append(item.text.strip())
+        comments = get_comments(html.findAll('div',id='lenta-card__text-review-escaped'))
             
         for j in range(marks.__len__()):
-            condidate = {names[j]:[comments[j],marks[j]]}
-            if condidate[names[j]][1] < 1 : 
-                zero+=1
-                dataset.update(condidate)
-            elif condidate[names[j]][1] < 2 : 
-                one+=1
-                dataset.update(condidate)
-            elif condidate[names[j]][1] < 3 : 
-                two+=1
-                dataset.update(condidate)
-            elif condidate[names[j]][1] < 4 : 
-                three+=1
-                dataset.update(condidate)
-            elif condidate[names[j]][1] < 5 : 
-                four+=1
-                dataset.update(condidate)
-            elif condidate[names[j]][1] == 5 : 
-                five+=1
-                dataset.update(condidate)
-    
-    """
-    print(marks)
-    
-    print(names)
-    
-    outFile = open('output.xml', 'wb')
-    for comment in comments:
-        outFile.write(comment.encode('utf-8'))
-        outFile.write('\n\n\n'.encode('utf-8'))
-    """
-    
-    
-    
+            condidate = Comment(names[j],comments[j],float(marks[j]))
             
+            if condidate.mark <= 1.0 : 
+                one+=1
+                dataset.append(condidate)
+                
+            elif condidate.mark <= 2.0 : 
+                two+=1
+                dataset.append(condidate)
+                
+            elif condidate.mark <= 3.0 : 
+                three+=1
+                dataset.append(condidate)
+                
+            elif condidate.mark <= 4.0 : 
+                four+=1
+                dataset.append(condidate)
+                
+            elif condidate.mark <= 5.0 : 
+                five+=1
+                dataset.append(condidate)
     
+    create_repo()
+    
+    counter = 0
+    
+    for comment in dataset:
+        filename = 'C:\\Users\\RELOAD\\Desktop\\Уник\\прикладное пр-е\\FirstLabParser\\Application-programming\\dataset\\'+str(comment.mark[0])
+        file = open(filename,'wb+')
+        get_last_file_name(filename)
         
+        file.close()
